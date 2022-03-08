cf6181308545b3f9fe545f6eda19a08faef6d8bc
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-03-07 20:36:19 -0800

    third commit

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -3,8 +3,13 @@ const breads = express.Router()
 const Bread = require('../models/bread.js');
 
 // INDEX
+breads.get('/', (req, res) => {
+    res.render('index', {breads: Bread})
+})
+
+//SHOW
 breads.get('/:arrayIndex', (req, res) => {
-  res.send(Bread[req.params.arrayIndex])
+    res.send(Bread[req.params.arrayIndex])
 })
 
 module.exports = breads
